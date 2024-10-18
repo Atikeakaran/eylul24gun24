5b5a8136530dcd6a37a5a02b4c43a284567b95fe
Atikeakaran <atikeakaran@gmail.com>
2024-10-18 21:36:18 +0300

    diziler

---
+++ dizidongu.js
@@ -0,0 +1,45 @@
+let dizi = ["Betül", "Seviye", "Güneş", "Ati", "Nur", "Gürkan", "Burak"];
+
+//dizi birleştirme
+let birlesik = dizi.join("<>");
+console.log(birlesik);
+
+//dizi üzerinde dönme yöntemleri
+
+// 1:for
+for (let i = 0; i < dizi.length; i++)
+    console.log(i + " " + dizi[i]);
+
+console.log("-----------------------------");
+
+//2. for .. in
+for (const i in dizi)
+    console.log(i + " " + dizi[i]);
+console.log("-----------------------------");
+
+//3. for .. of
+for (let oge of dizi)
+    console.log(oge);
+console.log("-----------------------------");
+
+//4. forEach method
+dizi.forEach(function (value, index) {
+    console.log(index + " : " + value);
+});
+console.log("-----------------------------");
+
+//4.1 forEach method kısa yazımı
+dizi.forEach((v, i) => console.log(i + " > " + v));
+console.log("-----------------------------");
+
+//5 while 
+let sayac = 0;
+while (sayac < dizi.length)
+    console.log(sayac + " / " + dizi[sayac++]);
+console.log("-----------------------------");
+
+// dizi ters çevirme
+for(let i= dizi.length -1; i>=0; i--){
+    let oge = dizi[i];
+    console.log(i + " " +oge)
+}
